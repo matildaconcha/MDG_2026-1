473d60b8b08b509ee990ffbf212f57c141fd36da
matildaconcha <matilda.concha@usach.cl>
2026-07-08 17:35:52 -0400

    Contenido sexta clase sql y qgis

--- datos/SQL/Clase_CENSO.sql
+++ datos/SQL/Clase_CENSO.sql
@@ -1,3 +1,4 @@
+
 --Total de personas por comuna
 
 SELECT com.nom_comuna AS "COMUNA", COUNT(*) AS "Total"
@@ -86,3 +87,35 @@ JOIN comunas as com
 ON zon."codigo_comuna" = com."codigo_comuna"
 GROUP BY nom_comuna, zon.geocodigo
 ORDER BY "TOTAL_PROFESIONALES" DESC;
+
+-- Tasa de profesionales por zona censal, ordenados de mayor a menor
+CREATE TABLE tablas_cpm.tot_profesionales AS
+SELECT z.geocodigo, 
+    c.nom_comuna, 
+    ROUND(COUNT(*) FILTER (WHERE p.p15 >= 12 AND p.p15 <= 14) * 100.0 / 
+    COUNT(*) FILTER (WHERE p.p09 > 18),2) AS total_profesionales
+FROM personas AS p
+JOIN hogares AS h
+ON p.hogar_ref_id = h.hogar_ref_id 
+JOIN viviendas AS v                  
+ON h.vivienda_ref_id = v.vivienda_ref_id
+JOIN zonas AS z
+ON v.zonaloc_ref_id = z.zonaloc_ref_id
+JOIN comunas AS c
+ON z.codigo_comuna = c.codigo_comuna
+GROUP BY z.geocodigo, c.nom_comuna
+ORDER BY total_profesionales DESC;
+
+-- Se une la geometría a la tabla de profesionales
+CREATE TABLE tablas_cpm.tot_prof_geom AS
+SELECT tp.geocodigo,
+		tp.nom_comuna,
+		tp.total_profesionales AS tasa_prof,
+		zona.geom 
+FROM tablas_cpm.tot_profesionales AS tp
+JOIN dpa.zonas_censales_v AS zona
+ON tp.geocodigo::double precision = zona.geocodigo;
+
+-- Acceder a sistema de coordenadas
+SELECT ST_SRID(geom)
+FROM tablas_cpm.tot_prof_geom
